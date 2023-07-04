8ad491dac37fe179580b2223e9d03df6b3e00ee3
MuskanMall <muskanmall22@gmail.com>
2023-07-04 12:27:19 +0530

    Extra customization
    Added some extra functionalities/restrictions

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ passwordEl1 =  document.querySelector("#password-el1");
 passwordEl2 = document.querySelector("#password-el2");
 displayLimit = document.querySelector("#display-limit");
 limit = document.querySelector("#limit");
+alph = document.querySelector("#alphabets");
+num = document.querySelector("#numbers");
+chars = document.querySelector("#characters");
 
 document.querySelector("#input-length").addEventListener("click", function(event){
     event.preventDefault()
     getLimit()
 })
+
 let l = null
 function getLimit(){
    l = limit.value
-   console.log(l);
    l = parseInt(l)
    displayLimit.textContent = l
 }
@@ -22,6 +25,7 @@ function getLimit(){
 let password1= ""
 let password2 = ""
 len = characters.length;
+console.log(len);
 function generatePassword(){
     password1= ""
     password2 = ""
@@ -29,11 +33,55 @@ function generatePassword(){
         l = 15
     }
     console.log(l)
-    for (let i = 0; i<l; i++){
-        password1 += characters[Math.floor(Math.random() * len)]
-        password2 += characters[Math.floor(Math.random() * len)]
-        
+    if ((alph.checked && num.checked && chars.checked) || (alph.checked == false && num.checked==false && chars.checked== false)){
+        for (let i = 0; i<l; i++){
+            password1 += characters[Math.floor(Math.random() * len)]
+            password2 += characters[Math.floor(Math.random() * len)]
+        }
     }
+    else if(alph.checked && num.checked==false && chars.checked== false){
+        for (let i = 0; i<l; i++){
+            alphabet = characters.slice(0,52)
+            password1 += alphabet[Math.floor(Math.random() * 52)]
+            password2 += alphabet[Math.floor(Math.random() * 52)]
+        }
+    }
+    else if(alph.checked == false && num.checked && chars.checked== false){
+        for (let i = 0; i<l; i++){
+            nums = characters.slice(52,62)
+            password1 += nums[Math.floor(Math.random() * 10)]
+            password2 += nums[Math.floor(Math.random() * 10)]
+        }
+    }
+    else if(alph.checked == false && num.checked==false && chars.checked){
+        for (let i = 0; i<l; i++){
+            charas = characters.slice(62)
+            password1 += charas[Math.floor(Math.random() * 29)]
+            password2 += charas[Math.floor(Math.random() * 29)]
+        }
+    }
+    else if(chars.checked && num.checked){
+        for (let i = 0; i<l; i++){
+            nac = characters.slice(52)
+            password1 += nac[Math.floor(Math.random() * 39)]
+            password2 += nac[Math.floor(Math.random() * 39)]
+        }
+    }
+    else if(alph.checked && num.checked){
+        for (let i = 0; i<l; i++){
+            naa = characters.slice(0,62)
+            password1 += naa[Math.floor(Math.random() * 62)]
+            password2 += naa[Math.floor(Math.random() * 62)]
+        }
+    }
+    else if(alph.checked && chars.checked){
+        for (let i = 0; i<l; i++){
+            caa = characters.slice(0,52).concat(characters.slice(62,91))
+            password1 += caa[Math.floor(Math.random() * 81)]
+            password2 += caa[Math.floor(Math.random() * 81)]
+        }
+    }
+
     passwordEl1.textContent = password1
     passwordEl2.textContent = password2
     
